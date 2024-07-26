6296dc829329c0d17df00339a0d84f4b3803536d
MinjuKang <minjukang727@gmail.com>
2024-07-26 19:57:43 +0900

    Level 1. 요구사항 2. Scanner를 사용하여 사칙연산 기호를 전달받을 수 있습니다. - 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. (`charAt(0)`) --------------------------------------------------------------------------- Comment : 사칙 연산 기호는 '+', '-', '*', '/'으로 모두 한 문자입니다. Scanner 객체 sc에 nextLine().charAt(0)를 이용하면 입력받은 사칙연산 기호를 가져올 수 있습니다. 입력받은 사칙연산 기호를 저장할 수 있는 변수의 타입으로는 char와 String이 있습니다.
    - char : 1byte, 한 문자 한 개만 저장합니다.
      - 문자 비교: 비교연산자 '=='를 이용할 수 있습니다.
    - String : 문자 여러개를 문장 형태로 저장합니다.
      - 문자열 비교 : String은 참조형 변수로 비교 연산자 '=='를 사용하면 문자열을 비교하는 것이 아닌 메모리  주소값을 비교합니다.
        String 변수의 문자열 비교는 equals() 메서드를 사용합니다.
    
    char형과 String형 변수 모두 문자를 담을 수 있지만
    나중에 문자의 비교를 할 때, 비교연산자 '=='를 이용하는 것이 더 편하게
    느껴져서 char형 변수에 사칙연산 기호를 할당하였습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int n1 = sc.nextInt();
+
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int n2 = sc.nextInt();
         sc.nextLine();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        char op = sc.nextLine().charAt(0);
     }
 }
