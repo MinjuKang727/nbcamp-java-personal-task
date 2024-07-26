fa62fd4e05bf0efecb951a077f59a272cf388480
MinjuKang <minjukang727@gmail.com>
2024-07-26 20:01:45 +0900

    Level 1. 요구사항 3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.   - 사칙연산 기호에 맞는 연산자를 사용하여 연산을 진행합니다.   - 입력 받은 연산 기호를 구분하기 위해 제어문을 사용합니다. (e.g.if, switch)   - 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.     - e.g. "나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다." ----------------------------------------------------------------------------------- Comment : 입력받은 연산 기호를 구분하기 위한 제어문으로 if문과 switch case 문 중 고민하였는데 인강에서 switch case문이 더 가독성이 좋다고 하셨고 switch case문을 작성한 경험이 별로 없어서 연습 삼아 switch case문을 사용해 보았습니다.
    처음에는 연산 결과를 담을 변수의 타입을 정수형 변수 result 하나만 초기화 해 두었었는데
    나눗셈 연산의 결과에서는 소숫점 아래 값이 나올 수 있기 때문에 실수형 변수 resultf를 하나 더 초기화 해 주었습니다.
    
    다음으로 연산자 기호를 담은 char 변수 op에 대하여 switch case문을 작성해 주었습니다.
    case '+' 또는 '-' 또는 '*'의 경우, 정수형 변수 result에 계산 결과를 할당하고 출력해 주었습니다.
    case '/'의 경우, resultf에 나눗셈 결과를 할당하고 출력해 주었는데
    그냥 n1 / n2를 하게 되면 n1, n2가 모두 정수형이라 결과가 정수형으로 나오게 되므로
    n1 앞에 (float)을 붙여 n1을 실수로 형변환 후, 나눗셈을 하여 결과값이 실수로 나올 수 있도록 하였습니다.
    
    연산을 진행할 때, 발생할 수 있는 연산 오류는 다음과 같습니다.
    - 입력값 오류
      - 첫번째, 두번째 입력값에 정수가 아닌 값을 입력할 경우
        >> try - catch문으로 Exception 발생할 경우 에러 메세지 출력
      - 연산 기호를 입력하지 않고 Enter를 할 경우
        : 문자를 입력하지 않아도 값을 가져와서 nextLine().charAt(0); 코드 수정
        >> next().charAt(0);
      - 사칙연산 기호가 아닌 기호를 입력하였을 경우
        >> switch case문의 default를 실행하여 에러 메세지 출력
      - 나눗셈 연산에서 두번째 정수가 0인 경우
        : 나눗셈에서 나누는 수는 0이 될 수 없습니다.
        >> case '/' 에서 조건문을 실행하여 n2 == 0인 경우, 에러 메세지 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,22 +3,53 @@ package calculator;
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        try{
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int n1 = sc.nextInt();
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int n1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int n2 = sc.nextInt();
+            sc.nextLine();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int n2 = sc.nextInt();
-        sc.nextLine();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            char op = sc.next().charAt(0);
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        char op = sc.nextLine().charAt(0);
+            int result = 0;
+            float resultf = 0;
+
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            switch (op) {
+                case '+' :
+                    result = n1 + n2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '-' :
+                    result = n1 - n2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '*' :
+                    result = n1 * n2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '/' :
+                    if (n2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    } else {
+                        resultf = (float) n1 / n2;
+                        System.out.println("결과: " + resultf);
+                    }
+                    break;
+                default :
+                    System.out.println("알 수 없는 사칙연산 기호를 입력하셨습니다.");
+            }
+        } catch (Exception e) {
+            System.out.println("알 수 없는 값을 입력하셨습니다.");
+        }
     }
 }
